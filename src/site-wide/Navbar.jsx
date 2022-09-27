c1a4825637180ef5ce4e67994c1ef025b26791f6
olivershh <oliver.shackleton@gmail.com>
2022-09-27 14:31:28 +0100

    changed color of home link

--- src/site-wide/Navbar.jsx
+++ src/site-wide/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav className="nav-site-wide">
-            <Link to="/">home</Link>
+            <Link className="navbar-home-link" to="/">home</Link>
         </nav>
     )
 }
